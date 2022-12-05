754fe4302fb8ac7865a928b3af5d705ec8d8e73d
bryankahl28 <114097155+bryankahl28@users.noreply.github.com>
2022-12-04 21:51:36 -0500

    Add files via upload

---
+++ PythonQueue.py
@@ -0,0 +1,11 @@
+names = []
+for x in range (10):
+    name = input ("Enter names: ")
+    names.append(name)
+print(names)
+
+for x in range (10):
+    len(names)
+    print (names.pop(0))
+
+print(names)
